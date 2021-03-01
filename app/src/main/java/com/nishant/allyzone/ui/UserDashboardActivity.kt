4567a0f00e546248951c7280dd8e8a105c4d707a
Nishant <official.nishant1115@gmail.com>
2021-03-01 19:23:41 +0530

    Bottom Navigation Bar changed

--- app/src/main/java/com/nishant/allyzone/ui/UserDashboardActivity.kt
+++ app/src/main/java/com/nishant/allyzone/ui/UserDashboardActivity.kt
@@ -1,19 +1,32 @@
 package com.nishant.allyzone.ui
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
 import com.nishant.allyzone.R
 import kotlinx.android.synthetic.main.activity_user_dashboard.*
 
 class UserDashboardActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_dashboard)
 
-        bottomNavigationView.setupWithNavController(findNavController(R.id.fragment))
+        navController = findNavController(R.id.fragment)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_bottom_nav, menu)
+        bottomNavigationView.setupWithNavController(menu, navController)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController.navigateUp()
+        return true
     }
 }
